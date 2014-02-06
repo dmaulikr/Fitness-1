8a063bc0352518d5febb93e1db19a9f7ada6de81
prashantgohel1 <prashantgohel1@gmail.com>
2014-02-06 17:18:26 +0530

    Empty Project
    This is the first time, when the project folder is created.

---
+++ Fitness/ISAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ISAppDelegate.h
+//  Fitness
+//
+//  Created by ispluser on 2/6/14.
+//  Copyright (c) 2014 ISC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
