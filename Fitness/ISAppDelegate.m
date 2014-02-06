f6bcf5535d22db3eea7fadcec8d121ef667d1d8c
prashantgohel1 <prashantgohel1@gmail.com>
2014-02-06 17:22:04 +0530

    The first change
    not important

--- Fitness/ISAppDelegate.m
+++ Fitness/ISAppDelegate.m
@@ -13,7 +13,11 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
+    // Override point for customization after application launch.]
+    
+    
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
