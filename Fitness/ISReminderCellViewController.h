23db9982c940d352a3ab4f98891022fd7342ebf3
prashantgohel1 <prashantgohel1@gmail.com>
2014-02-13 15:24:57 +0530

    in-between reminder cell

---
+++ Fitness/ISReminderCellViewController.h
@@ -0,0 +1,13 @@
+//
+//  ISReminderCellViewController.h
+//  Fitness
+//
+//  Created by ispluser on 2/13/14.
+//  Copyright (c) 2014 ISC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ISReminderCellViewController : UIViewController
+
+@end
